33b3d50767e367c6372354662791cba6a8453f7c
Takayasu Beharu <info@jumpeer.jp>
2023-04-22 13:29:50 +0900

    [IN PROGRESS] 'get_user_name' 85%

--- src/api/get_user_name.test.ts
+++ src/api/get_user_name.test.ts
@@ -1,3 +1,34 @@
 import {getUserName} from "@/api/get_user_name";
 import {fetchUser} from "@/api/fetch_user";
 
+import {createUser} from "@/api/create_user";
+
+import {resetDB} from "@/api/db/reset_db";
+import {databaseManager} from "@/api/db";
+import {User} from "./models/user";
+
+
+
+describe("getUserName", () => {
+  beforeEach(async () => {
+    const db = await databaseManager.getInstance();
+    await resetDB(db);
+    // await createUser("John Doe", "xxx@email.com");
+  });
+
+  it("create user", async () => {
+    const db = await databaseManager.getInstance();
+    await resetDB(db);
+    await createUser("Taka", "xxx@email.com");
+
+    const user = await fetchUser(1);
+    const name = await getUserName(1);
+    expect(name).toBe(user.name);
+  });
+  // it("should return the user's name", async () => {
+  //   const user = await fetchUser(0);
+  //   expect(user.name).toBe("John Doe");
+  //   // const name = await getUserName(1);
+  //   // expect(name).toBe("John Doe");
+  // });
+});
