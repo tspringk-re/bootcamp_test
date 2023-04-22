8f8ce4f86b66d23fce99d3d429f2889ae1001044
Takayasu Beharu <info@jumpeer.jp>
2023-04-22 12:47:56 +0900

    [DONE] 'isValidDate' done

--- src/is_valid_date/index.test.ts
+++ src/is_valid_date/index.test.ts
@@ -1,2 +1,7 @@
 import {isValidDate} from "@/is_valid_date";
 
+describe("isValidDate", () => {
+  it("returns true if date is valid", () => {
+    expect(isValidDate(2021, 1, 1)).toBe(true);
+  });
+});
