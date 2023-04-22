936ecd591fd04dd5b68a9a4b61bdbe55bd225379
Takayasu Beharu <info@jumpeer.jp>
2023-04-22 14:21:15 +0900

    [DONE] 'get_user_name' done

--- src/api/get_user_name.test.ts
+++ src/api/get_user_name.test.ts
@@ -13,22 +13,36 @@ describe("getUserName", () => {
   beforeEach(async () => {
     const db = await databaseManager.getInstance();
     await resetDB(db);
-    // await createUser("John Doe", "xxx@email.com");
   });
 
-  it("create user", async () => {
-    const db = await databaseManager.getInstance();
-    await resetDB(db);
+  // createUser Test
+  it("Create user with valid arguments", async () => {
     await createUser("Taka", "xxx@email.com");
-
-    const user = await fetchUser(1);
-    const name = await getUserName(1);
-    expect(name).toBe(user.name);
   });
-  // it("should return the user's name", async () => {
-  //   const user = await fetchUser(0);
-  //   expect(user.name).toBe("John Doe");
-  //   // const name = await getUserName(1);
-  //   // expect(name).toBe("John Doe");
-  // });
+
+  it("Create user with invalid arguments", async () => {
+    await createUser("", "");
+  });
+
+  // fetchUser Test
+  it("Fetch user with exist user-id", async () => {
+    await createUser("Taka", "xxx@email.com");
+    const user = await fetchUser(1);
+    expect(user).toBeInstanceOf(User);
+  });
+  it("fetch user with not exist user-id", async () => {
+    const user = await fetchUser(0);
+    expect(user).toBeInstanceOf(Error);
+  });
+
+  // getUserName Test
+  it("Get user name with exist user-id", async () => {
+    await createUser("Taka", "xxx@email.com");
+    const name = await getUserName(1);
+    expect(name).toBe("Taka");
+  });
+  it("Get user name with not exist user-id", async () => {
+    const name = await getUserName(0);
+    expect(name).toBeInstanceOf(Error);
+  });
 });
